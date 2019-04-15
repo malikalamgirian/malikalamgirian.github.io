754714a68bb6f307815fccf197466ddc05cc8920
Wasif Altaf <malikalamgirian@gmail.com>
2019-04-15 04:11:57 +0200

    Updated nightmode

---
+++ res/js/accessibility.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+
+            # when clicking light mode button
+            $( "#lightModeBtnnnnn" ).click(function() {
+              $(this).hide();
+              alert( "Handler for .lightModeBtn() called." );
+            });
+
+
+            # when clicking dark mode button
+            $( "#darkModeBtnnnn" ).click(function() {
+              $(this).hide();
+              alert( "Handler for .darkModeBtn() called." );
+            });
+
+
+
+});
